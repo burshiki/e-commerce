d02d8f39d0957106f490fc50642961805c4ef145
Louie John Manuel Lauro <burshiki@gmail.com>
2022-09-06 02:16:40 +0800

    initialize files

---
+++ src/layouts/admin/MasterLayout.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Footer from './Footer'
+
+import '../../assets/admin/css/styles.css'
+import '../../assets/admin/js/scripts.js'
+
+const MasterLayout = () => {
+  return (
+    <div className="sb-nav-fixed">
+        <Navbar />
+        <div id="layoutSidenav">
+
+            <div id="layoutSidenav_nav">
+                <Sidebar />
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                </main>
+                <Footer />
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default MasterLayout
