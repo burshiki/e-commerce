6176ec38e9d0557b094dfcf048e74850088e5c7b
Louie John Manuel Lauro <burshiki@gmail.com>
2022-09-07 03:07:29 +0800

    update routes

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -1,17 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+
 import Dashboard from '../components/admin/Dashboard'
 import Profile from '../components/admin/Profile'
 import MasterLayout from '../layouts/admin/MasterLayout'
 
-export function AdminRoutes() {
+
+export default function AdminRoutes() {
     return (
-        
+      <>
         <Routes>
-            <Route path="/admin" element={<MasterLayout />} >
-                <Route path="/admin/dashboard" element={ <Dashboard /> } />
-                <Route path="/admin/profile" element={ <Profile /> } />
-                <Route path="/admin" element={<Navigate to="/admin/dashboard" /> } /> 
+            <Route element={<MasterLayout />} >
+                <Route path="dashboard" element={ <Dashboard /> } />
+                <Route path="profile" element={ <Profile /> } />
+                <Route path="" element={<Navigate to="/admin/dashboard" /> } /> 
             </Route>
         </Routes>
+      </>  
     )
 }
